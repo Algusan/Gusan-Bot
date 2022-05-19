bea087ff7aa817f4400bfbcbe017375505be62c3
Algusan <104574577+Algusan@users.noreply.github.com>
2022-05-19 14:11:43 +0300

    Add files via upload

--- src/chitoge.ts
+++ src/chitoge.ts
@@ -12,13 +12,11 @@ import CallHandler from "./Handlers/CallHandler";
 import AssetHandler from "./Handlers/AssetHandler";
 import EventHandler from "./Handlers/EventHandler";
 import NewsHandler from "./Handlers/NewsHandler";
-import ModsHandler from "./Handlers/ModsHandler";
-import ModsChecker from "./Checkers/ModsChecker";
 
 if (!process.env.MONGO_URI) throw new Error("MONGO URL IS NOT PROVIDED");
 const client = new WAClient({
-  name: process.env.NAME || "Chitoge",
-  session: process.env.SESSION || "Chitoge",
+  name: process.env.NAME || "Gusan",
+  session: process.env.SESSION || "Gusan",
   prefix: process.env.PREFIX || ":",
   mods: (process.env.MODS || "")
     .split(", ")
@@ -37,8 +35,6 @@ const callHandler = new CallHandler(client);
 const assetHandler = new AssetHandler(client);
 const eventHandler = new EventHandler(client);
 const newsHandler = new NewsHandler(client);
-const modsHandler = new ModsHandler(client);
-const modsChecker = new ModsChecker(client);
 messageHandler.loadCommands();
 assetHandler.loadAssets();
 messageHandler.loadFeatures();
@@ -116,7 +112,6 @@ db.once("open", () => {
     messageHandler.handleState();
     newsHandler.broadcastNews();
     messageHandler.spawnPokemon();
-    modsChecker.check();
     messageHandler.summonChara();
   });
 });
